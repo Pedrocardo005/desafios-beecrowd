6f7617131a1162660d5e063b4a89da9e0c4612e2
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-03-25 21:57:36 -0300

    Resolve 1018

--- src/App.java
+++ src/App.java
@@ -1,53 +1,28 @@
-import java.util.Locale;
 import java.util.Scanner;
 
 public class App {
+
+    static int busca(int entrada, int divisor) {
+		if (entrada % divisor != 0 || entrada % divisor == 0) {
+			int valor = 0;
+			
+			valor = (int) entrada / divisor;
+			System.out.printf("%d nota(s) de R$ %d,00%n", valor, divisor);
+			entrada = entrada % divisor;
+		}
+		return entrada;
+	}
+
     public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
-        scanner.useLocale(Locale.US);
-        Locale.setDefault(Locale.US);
-        
-        Double num1, num2, num3, num4, media;
-
-        num1 = scanner.nextDouble();
-        num2 = scanner.nextDouble();
-        num3 = scanner.nextDouble();
-        num4 = scanner.nextDouble();
-
-        num1 *= 0.2;
-        num2 *= 0.3;
-        num3 *= 0.4;
-        num4 *= 0.1;
-
-        media = num1 + num2 + num3 + num4;
-
-        String mediaFormated = media.toString();
-
-        mediaFormated = mediaFormated.substring(0, 3);
-
-        System.out.println("Media: " + mediaFormated);
-
-        if (media >= 7) {
-            System.out.println("Aluno aprovado.");
-        } else if (media >= 5) {
-            double notaExame = 0.0;
-
-            System.out.println("Aluno em exame.");
-
-            notaExame = scanner.nextDouble();
-            System.out.printf("Nota do exame: %.1f%n", notaExame);
-
-            media = (media + notaExame) / 2;
-
-            if (media >= 5) {
-                System.out.println("Aluno aprovado.");
-            } else {
-                System.out.println("Aluno reprovado.");
-            }
-
-            System.out.printf("Media final: %.1f%n", media);
-        } else {
-            System.out.println("Aluno reprovado.");
-        }
-    }
+        Scanner sc = new Scanner(System.in);
+		int entrada = 0;
+		entrada = sc.nextInt();
+		int[] listagem = {100, 50, 20, 10, 5, 2, 1};
+		
+        System.out.println(entrada);
+		for (int i = 0; i < listagem.length; i++) {
+			entrada = busca(entrada, listagem[i]);
+		}
+ 
+    }   
 }
