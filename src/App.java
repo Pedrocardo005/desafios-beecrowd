60c50945a119fb721b3ee5407285c53a740c68eb
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-03-28 22:58:11 -0300

    Desafio 1044 nao resolvido

--- src/App.java
+++ src/App.java
@@ -1,26 +1,18 @@
-import java.util.Locale;
 import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-		sc.useLocale(Locale.US);
-		Locale.setDefault(Locale.US);
-		double a, b, c;
+		int numA, numB;
 
-		a = sc.nextDouble();
-		b = sc.nextDouble();
-		c = sc.nextDouble();
+		numA = sc.nextInt();
+		numB = sc.nextInt();
 
-		if (a + b > c && a + c > b && c + b > a) {
-			double perimetro = a + b + c;
-
-			System.out.printf("Perimetro = %.1f%n", perimetro);
+		if (numB % numA == 0) {
+			System.out.println("Sao Multiplos");
 		} else {
-			double area = ((a + b) * c) / 2;
-
-			System.out.printf("Area = %.1f%n", area);
+			System.out.println("Nao sao Multiplos");
 		}
-    }   
+    }
 }
