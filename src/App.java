c18292a3be70e41bdfb5faf8c09bb9524814edf3
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-04-01 23:59:49 -0300

    Tentando resolver a 1047

--- src/App.java
+++ src/App.java
@@ -4,12 +4,26 @@ public class App {
 
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-		int horaInicial, minutoInicial, horaFinal, minutoFinal;
+		int horaInicial, minutoInicial, horaFinal, minutoFinal, 
+			horaMinutoInicial, horaMinutoFinal, diferenca,
+			minutosSobra, horas;
 
 		horaInicial = sc.nextInt();
 		minutoInicial = sc.nextInt();
 		horaFinal = sc.nextInt();
 		minutoFinal = sc.nextInt();
 
+		if (horaFinal < horaInicial && minutoFinal < minutoInicial) {
+			horaFinal += 24;
+		}
+
+		horaMinutoInicial = (horaInicial * 60) + minutoInicial;
+		horaMinutoFinal = (horaFinal * 60) + minutoFinal;
+
+		diferenca = horaMinutoFinal - horaMinutoInicial;
+		minutosSobra = diferenca % 60;
+		horas = diferenca / 60;
+
+		System.out.printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)%n", horas, minutosSobra);
     }
 }
