64c1e9269894e6b4c0417035d28bf6f263539d45
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-04-04 22:52:39 -0300

    Resolve 1049

--- src/App.java
+++ src/App.java
@@ -1,57 +1,46 @@
-import java.util.Locale;
 import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        Locale.setDefault(Locale.US);
-        sc.useLocale(Locale.US);
+        
+        String nothingWord, firstWord, secondWord;
 
-        double salario = 0.0;
+        nothingWord = sc.nextLine();
+        firstWord = sc.nextLine();
+        secondWord = sc.nextLine();
 
-        salario = sc.nextDouble();
+        if (firstWord.equals("ave")) {
+            if (secondWord.equals("carnivoro")) {
+                System.out.println("aguia");
+            } else {
+                System.out.println("pomba");
+            }
+        }
 
-        if (salario >= 0 && salario <= 400.00) {
-            double novoSalario, reajuste = salario * 0.15;
-            
-            novoSalario = salario + reajuste;
+        if (firstWord.equals("mamifero")) {
+            if (secondWord.equals("onivoro")) {
+                System.out.println("homem");
+            } else {
+                System.out.println("vaca");
+            }
+        }
 
-            System.out.printf("Novo salario: %.2f%n", novoSalario);
-            System.out.printf("Reajuste ganho: %.2f%n", reajuste);
-            System.out.println("Em percentual: 15 %");
-        } else if (salario >= 400.01 && salario <= 800.00) {
-            double novoSalario, reajuste = salario * 0.12;
-            
-            novoSalario = salario + reajuste;
+        if (firstWord.equals("inseto")) {
+            if (secondWord.equals("hematofago")) {
+                System.out.println("pulga");
+            } else {
+                System.out.println("lagarta");
+            }
+        }
 
-            System.out.printf("Novo salario: %.2f%n", novoSalario);
-            System.out.printf("Reajuste ganho: %.2f%n", reajuste);
-            System.out.println("Em percentual: 12 %");
-        } else if (salario >= 800.01 && salario <= 1200.00) {
-            double novoSalario, reajuste = salario * 0.10;
-            
-            novoSalario = salario + reajuste;
-
-            System.out.printf("Novo salario: %.2f%n", novoSalario);
-            System.out.printf("Reajuste ganho: %.2f%n", reajuste);
-            System.out.println("Em percentual: 10 %");
-        } else if (salario >= 1200.01 && salario <= 2000.00) {
-            double novoSalario, reajuste = salario * 0.07;
-            
-            novoSalario = salario + reajuste;
-
-            System.out.printf("Novo salario: %.2f%n", novoSalario);
-            System.out.printf("Reajuste ganho: %.2f%n", reajuste);
-            System.out.println("Em percentual: 7 %");
-        } else if (salario > 2000.00) {
-            double novoSalario, reajuste = salario * 0.04;
-            
-            novoSalario = salario + reajuste;
-
-            System.out.printf("Novo salario: %.2f%n", novoSalario);
-            System.out.printf("Reajuste ganho: %.2f%n", reajuste);
-            System.out.println("Em percentual: 4 %");
+        if (firstWord.equals("anelideo")) {
+            if (secondWord.equals("hematofago")) {
+                System.out.println("sanguessuga");
+            } else {
+                System.out.println("minhoca");
+            }
         }
     }
 }
