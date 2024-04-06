26017cd760114f1dc7320578a4e3a5245049938d
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-04-05 23:42:26 -0300

    Resolve 1050

--- src/App.java
+++ src/App.java
@@ -4,43 +4,36 @@ public class App {
 
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
+        int ddd = sc.nextInt();
         
-        String nothingWord, firstWord, secondWord;
-
-        nothingWord = sc.nextLine();
-        firstWord = sc.nextLine();
-        secondWord = sc.nextLine();
-
-        if (firstWord.equals("ave")) {
-            if (secondWord.equals("carnivoro")) {
-                System.out.println("aguia");
-            } else {
-                System.out.println("pomba");
-            }
-        }
-
-        if (firstWord.equals("mamifero")) {
-            if (secondWord.equals("onivoro")) {
-                System.out.println("homem");
-            } else {
-                System.out.println("vaca");
-            }
-        }
-
-        if (firstWord.equals("inseto")) {
-            if (secondWord.equals("hematofago")) {
-                System.out.println("pulga");
-            } else {
-                System.out.println("lagarta");
-            }
-        }
-
-        if (firstWord.equals("anelideo")) {
-            if (secondWord.equals("hematofago")) {
-                System.out.println("sanguessuga");
-            } else {
-                System.out.println("minhoca");
-            }
+        switch(ddd) {
+            case 61:
+                System.out.println("Brasilia");
+                break;
+            case 71:
+                System.out.println("Salvador");
+                break;
+            case 11:
+                System.out.println("Sao Paulo");
+                break;
+            case 21:
+                System.out.println("Rio de Janeiro");
+                break;
+            case 32:
+                System.out.println("Juiz de Fora");
+                break;
+            case 19:
+                System.out.println("Campinas");
+                break;
+            case 27:
+                System.out.println("Vitoria");
+                break;
+            case 31:
+                System.out.println("Belo Horizonte");
+                break;
+            default:
+                System.out.println("DDD nao cadastrado");
+                break;
         }
     }
 }
