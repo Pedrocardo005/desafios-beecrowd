3c9e1d1d4b5e73b5625bb3247f8ca67be92cc101
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-04-08 22:42:01 -0300

    Resolve 1059

--- src/App.java
+++ src/App.java
@@ -1,15 +1,10 @@
-import java.util.Scanner;
-
 public class App {
 
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int mes = sc.nextInt();
-        String[] meses = {"January", "February", "March", "April", "May", "June", "July", "August", "September",
-            "October",
-            "November",
-            "December"};
-        
-        System.out.println(meses[mes - 1]);
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
     }
 }
