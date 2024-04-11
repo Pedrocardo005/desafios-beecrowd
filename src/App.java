bd208a1657bdb3928096bdd8021623bc20c5d93f
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-04-10 23:32:03 -0300

    Resolve 1061

--- src/App.java
+++ src/App.java
@@ -6,8 +6,31 @@ public class App {
 
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        LocalDateTime startDateTime = LocalDateTime.of(2024, 1, 5, 8, 12, 23);
-        LocalDateTime endDateTime = LocalDateTime.of(2024, 1, 9, 6, 13, 23);
+
+        String dayStartString = sc.nextLine();
+        String hourStartString = sc.nextLine();
+
+        String dayFinishString = sc.nextLine();
+        String hourFinishString = sc.nextLine();
+
+        dayStartString = dayStartString.replaceAll("Dia ", "");
+        dayFinishString = dayFinishString.replaceAll("Dia ", "");
+
+        int day = Integer.parseInt(dayStartString);
+        int dayFinish = Integer.parseInt(dayFinishString);
+
+        String[] timeStrings = hourStartString.split(" : ");
+        int hour = Integer.parseInt(timeStrings[0]);
+        int minutes = Integer.parseInt(timeStrings[1]);
+        int seconds = Integer.parseInt(timeStrings[2]);
+
+        String[] timeFinishStrings = hourFinishString.split(" : ");
+        int hourFinish = Integer.parseInt(timeFinishStrings[0]);
+        int minutesFinish = Integer.parseInt(timeFinishStrings[1]);
+        int secondsFinish = Integer.parseInt(timeFinishStrings[2]);
+
+        LocalDateTime startDateTime = LocalDateTime.of(2024, 1, day, hour, minutes, seconds);
+        LocalDateTime endDateTime = LocalDateTime.of(2024, 1, dayFinish, hourFinish, minutesFinish, secondsFinish);
         Long valor = ChronoUnit.SECONDS.between(startDateTime, endDateTime);
 
         Long dias = valor / 86400;
