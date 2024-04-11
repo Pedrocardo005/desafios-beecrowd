caf03015321e24dc112b50c8a7b8823b519bf0cb
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-04-10 23:09:03 -0300

    Primeiras alteracoes para o 1061

--- src/App.java
+++ src/App.java
@@ -1,19 +1,25 @@
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        double[] lista = {0,0,0,0,0,0};
-        int positivos = 0;
+        LocalDateTime startDateTime = LocalDateTime.of(2024, 1, 5, 8, 12, 23);
+        LocalDateTime endDateTime = LocalDateTime.of(2024, 1, 9, 6, 13, 23);
+        Long valor = ChronoUnit.SECONDS.between(startDateTime, endDateTime);
 
-        for (int i = 0; i < lista.length; i++) {
-            lista[i] = sc.nextDouble();
-            if (lista[i] > 0) {
-                positivos++;
-            }
-        }
+        Long dias = valor / 86400;
+        Long horas = valor % 86400;
+        Long minutos = horas % 3600;
+        horas = horas / 3600;
+        Long segundos = minutos % 60;
+        minutos = minutos / 60;
 
-        System.out.println(positivos + " valores positivos");
+        System.out.println(dias + " dia(s)");
+        System.out.println(horas + " hora(s)");
+        System.out.println(minutos + " minuto(s)");
+        System.out.println(segundos + " segundo(s)");
     }
 }
