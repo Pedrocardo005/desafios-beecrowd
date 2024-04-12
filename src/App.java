8d6f288ed530e121f7e551a067e1add618a635c6
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-04-11 22:28:47 -0300

    Resolve a 1064

--- src/App.java
+++ src/App.java
@@ -1,48 +1,25 @@
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
+        int positivos = 0;
         Scanner sc = new Scanner(System.in);
+        double[] numeros = {0,0,0,0,0,0};
+        double media = 0;
 
-        String dayStartString = sc.nextLine();
-        String hourStartString = sc.nextLine();
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = sc.nextDouble();
 
-        String dayFinishString = sc.nextLine();
-        String hourFinishString = sc.nextLine();
+            if (numeros[i] > 0) {
+                positivos++;
+                media += numeros[i];
+            }
+        }
+        
+        media /= positivos;
 
-        dayStartString = dayStartString.replaceAll("Dia ", "");
-        dayFinishString = dayFinishString.replaceAll("Dia ", "");
-
-        int day = Integer.parseInt(dayStartString);
-        int dayFinish = Integer.parseInt(dayFinishString);
-
-        String[] timeStrings = hourStartString.split(" : ");
-        int hour = Integer.parseInt(timeStrings[0]);
-        int minutes = Integer.parseInt(timeStrings[1]);
-        int seconds = Integer.parseInt(timeStrings[2]);
-
-        String[] timeFinishStrings = hourFinishString.split(" : ");
-        int hourFinish = Integer.parseInt(timeFinishStrings[0]);
-        int minutesFinish = Integer.parseInt(timeFinishStrings[1]);
-        int secondsFinish = Integer.parseInt(timeFinishStrings[2]);
-
-        LocalDateTime startDateTime = LocalDateTime.of(2024, 1, day, hour, minutes, seconds);
-        LocalDateTime endDateTime = LocalDateTime.of(2024, 1, dayFinish, hourFinish, minutesFinish, secondsFinish);
-        Long valor = ChronoUnit.SECONDS.between(startDateTime, endDateTime);
-
-        Long dias = valor / 86400;
-        Long horas = valor % 86400;
-        Long minutos = horas % 3600;
-        horas = horas / 3600;
-        Long segundos = minutos % 60;
-        minutos = minutos / 60;
-
-        System.out.println(dias + " dia(s)");
-        System.out.println(horas + " hora(s)");
-        System.out.println(minutos + " minuto(s)");
-        System.out.println(segundos + " segundo(s)");
+        System.out.println(positivos + " valores positivos");
+        System.out.printf("%.1f%n", media);
     }
 }
