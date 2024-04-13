aaeb26c52dc8f693ee47bc840728f8c2211ff06e
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-04-12 23:54:08 -0300

    Resolve a 1065

--- src/App.java
+++ src/App.java
@@ -3,23 +3,18 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) throws Exception {
-        int positivos = 0;
         Scanner sc = new Scanner(System.in);
-        double[] numeros = {0,0,0,0,0,0};
-        double media = 0;
+        int pares = 0;
+        int[] numeros = {0,0,0,0,0};
 
         for (int i = 0; i < numeros.length; i++) {
-            numeros[i] = sc.nextDouble();
+            numeros[i] = sc.nextInt();
 
-            if (numeros[i] > 0) {
-                positivos++;
-                media += numeros[i];
+            if (numeros[i] % 2 == 0) {
+                pares++;
             }
         }
-        
-        media /= positivos;
 
-        System.out.println(positivos + " valores positivos");
-        System.out.printf("%.1f%n", media);
+        System.out.printf("%d valores pares%n", pares);
     }
 }
