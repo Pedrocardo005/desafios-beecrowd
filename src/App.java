f2a826a7d12c83efa38df8ee83e411bbbf495bd1
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-04-15 22:43:11 -0300

    Resolve a 1067

--- src/App.java
+++ src/App.java
@@ -4,31 +4,13 @@ public class App {
 
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        int pares = 0;
-        int impares = 0;
-        int positivos = 0;
-        int negativos = 0;
-        int[] numeros = {0,0,0,0,0};
 
-        for (int i = 0; i < numeros.length; i++) {
-            numeros[i] = sc.nextInt();
+        int numero = sc.nextInt();
 
-            if (numeros[i] % 2 == 0) {
-                pares++;
-            } else {
-                impares++;
-            }
-
-            if (numeros[i] > 0) {
-                positivos++;
-            } else if (numeros[i] < 0) {
-                negativos++;
-            }
+        for (int x = 1; x <= numero; x++) {
+            if (x % 2 != 0) {
+                System.out.println(x);
+            }            
         }
-
-        System.out.println(pares + " valor(es) par(es)");
-        System.out.println(impares + " valor(es) impar(es)");
-        System.out.println(positivos + " valor(es) positivo(s)");
-        System.out.println(negativos + " valor(es) negativo(s)");
     }
 }
