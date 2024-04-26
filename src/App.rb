c9d5791f7f5b4ddb3ea70451139ac3726b48fe06
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-04-25 22:16:52 -0300

    Resolve a 1074

--- src/App.rb
+++ src/App.rb
@@ -1,9 +1,34 @@
 number = gets.chomp
 number = number.to_i
 
+lista = [0]
+
 for i in 1..number
-  if i % 2 == 0
-    quadrado = i * i
-    puts "#{i}^2 = #{quadrado}"
+  number_buffer = gets.chomp
+  lista[i] = number_buffer.to_i
+end
+
+for i in 1..number
+  element = lista[i]
+
+  if element == 0
+    puts "NULL"
+    next
+  end
+
+  if element.even?
+    print "EVEN "
+    if element > 0
+      puts "POSITIVE"
+    else
+      puts "NEGATIVE"
+    end
+  else
+    print "ODD "
+    if element > 0
+      puts "POSITIVE"
+    else
+      puts "NEGATIVE"
+    end
   end
 end
