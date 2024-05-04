0f4c34ea88d030e2165c9f98084ed0cd14405d69
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-05-03 23:52:58 -0300

    Resolve a 1079

--- src/App.java
+++ src/App.java
@@ -1,16 +1,34 @@
+import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+        ArrayList<Double> numeros = new ArrayList<Double>();
         int n = 0;
 
         n = sc.nextInt();
+        sc.nextLine();
 
-        for (int i = 1; i < 11; i++) {
-            int multiplicacao = i * n;
-            System.out.printf("%d x %d = %d%n", i, n, multiplicacao);
+        for (int i = 0; i < n; i++) {
+            String lineString = sc.nextLine();
+
+            String[] listaStrings = lineString.split(" ");
+
+            double num1 = Double.parseDouble(listaStrings[0]);
+            double num2 = Double.parseDouble(listaStrings[1]);
+            double num3 = Double.parseDouble(listaStrings[2]);
+            double mediaPonderada = (num1 * 0.2) + (num2 * 0.3) + (num3 * 0.5);
+            numeros.add(mediaPonderada);
+        }
+
+        for (int index = 0; index < numeros.size(); index++) {
+            double numero = numeros.get(index);
+
+            System.out.printf("%.1f%n", numero);
         }
     }
 }
