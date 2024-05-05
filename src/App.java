942b19db2ed5f5a9a555b4db7c08761efc2c3acb
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-05-04 22:23:32 -0300

    Resolve a 1080

--- src/App.java
+++ src/App.java
@@ -1,34 +1,21 @@
-import java.util.ArrayList;
-import java.util.Locale;
 import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
-        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        ArrayList<Double> numeros = new ArrayList<Double>();
-        int n = 0;
+        int biggest = 0;
+        int biggestPosition = 0;
+        
+        for (int i = 1; i < 101; i++) {
+            int currentNumber = sc.nextInt();
 
-        n = sc.nextInt();
-        sc.nextLine();
-
-        for (int i = 0; i < n; i++) {
-            String lineString = sc.nextLine();
-
-            String[] listaStrings = lineString.split(" ");
-
-            double num1 = Double.parseDouble(listaStrings[0]);
-            double num2 = Double.parseDouble(listaStrings[1]);
-            double num3 = Double.parseDouble(listaStrings[2]);
-            double mediaPonderada = (num1 * 0.2) + (num2 * 0.3) + (num3 * 0.5);
-            numeros.add(mediaPonderada);
+            if (currentNumber > biggest) {
+                biggest = currentNumber;
+                biggestPosition = i;
+            }
         }
-
-        for (int index = 0; index < numeros.size(); index++) {
-            double numero = numeros.get(index);
-
-            System.out.printf("%.1f%n", numero);
-        }
+        System.out.println(biggest);
+        System.out.println(biggestPosition);
     }
 }
