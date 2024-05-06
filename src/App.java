8fdd51d54220f9c3d7aa3ff04e1cf1e67ec20373
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-05-05 22:47:05 -0300

    Resolve a 1094

--- src/App.java
+++ src/App.java
@@ -1,21 +1,57 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        int biggest = 0;
-        int biggestPosition = 0;
-        
-        for (int i = 1; i < 101; i++) {
-            int currentNumber = sc.nextInt();
+        double percentualSapos = 0;
+        double percentualRatos = 0;
+        double percentualCoelhos = 0;
+        int casos = 0;
+        int ratos = 0;
+        int sapos = 0;
+        int testes = 0;
+        int coelhos = 0;
 
-            if (currentNumber > biggest) {
-                biggest = currentNumber;
-                biggestPosition = i;
+        testes = sc.nextInt();
+        sc.nextLine();
+
+        for (int i = 0; i < testes; i++) {
+            String caso = sc.nextLine();
+            String[] informacoes = caso.split(" ");
+            int qtdCasos = Integer.parseInt(informacoes[0]);
+
+            casos += qtdCasos;
+
+            switch (informacoes[1]) {
+                case "C":
+                    coelhos += qtdCasos;
+                    break;
+
+                case "R":
+                    ratos += qtdCasos;
+                    break;
+            
+                case "S":
+                    sapos += qtdCasos;
+                    break;
+                default:
+                    break;
             }
         }
-        System.out.println(biggest);
-        System.out.println(biggestPosition);
+
+        percentualSapos = (double) (sapos * 100) / casos;
+        percentualRatos = (double) (ratos * 100) / casos;
+        percentualCoelhos = (double) (coelhos * 100) / casos;
+
+        System.out.printf("Total: %d cobaias%n", casos);
+        System.out.printf("Total de coelhos: %d%n", coelhos);
+        System.out.printf("Total de ratos: %d%n", ratos);
+        System.out.printf("Total de sapos: %d%n", sapos);
+        System.out.printf("Percentual de coelhos: %.2f %%%n", percentualCoelhos);
+        System.out.printf("Percentual de ratos: %.2f %%%n", percentualRatos);
+        System.out.printf("Percentual de sapos: %.2f %%%n", percentualSapos);
     }
 }
