ba674cd1a20474a6cbdc1ca8c183ff9af9a8e958
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-05-09 19:03:43 -0300

    Resolve a 1098
     - O java teve problemas para fazer somas consecutivas de numeros flutuante, por isso a divisao de inteiro para double na linha 11

--- src/App.java
+++ src/App.java
@@ -1,13 +1,22 @@
+import java.util.Locale;
+
 public class App {
 
     public static void main(String[] args) throws Exception {
-        int[] js = {7, 6, 5};
-        int i;
+        Locale.setDefault(Locale.US);
+        double[] js = {1, 2, 3};
+        double i;
 
-        for (i = 1; i <= 9; i += 2) {
+        for (i = 0; i <= 20; i += 2) {
+            double ii = i / 10.0;
             for (int j = 0; j < js.length; j++) {
-                System.out.printf("I=%d J=%d%n", i, js[j]);
-                js[j] += 2;
+                if ((int) ii == ii) {
+                    System.out.printf("I=%.0f J=%.0f%n", ii, js[j]);
+                } else {
+                    System.out.printf("I=%.1f J=%.1f%n", ii, js[j]);
+                }
+                
+                js[j] += 0.2;
             }
         }
     }
