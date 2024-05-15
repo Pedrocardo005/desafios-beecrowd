ab0c1706b16c92cb33d643803248b6acd836101c
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-05-14 22:40:41 -0300

    Resolve a 1114

--- src/App.java
+++ src/App.java
@@ -1,33 +1,19 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> results = new ArrayList<String>();
-        boolean isRuning = true;
-
-        while (isRuning) {
-            String lineString = sc.nextLine();
-            String[] pairString = lineString.split(" ");
-            int first = Integer.parseInt(pairString[0]);       
-            int second = Integer.parseInt(pairString[1]);
-            
-            if (first == second) {
+        
+        while (true) {
+            int valor = sc.nextInt();
+            sc.nextLine();
+            if (valor != 2002) {
+                System.out.println("Senha Invalida");
+            } else {
+                System.out.println("Acesso Permitido");
                 break;
             }
-            
-            if (first > second) {
-                results.add("Decrescente");
-            } else {
-                results.add("Crescente");
-            }
-
-        }
-
-        for (int i = 0; i < results.size(); i++) {
-            System.out.println(results.get(i));
         }
     }
 }
