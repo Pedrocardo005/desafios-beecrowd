a0242736446b12aa274333a6a5ef7991985b0e02
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-05-18 20:50:43 -0300

    Resolve a 1118

--- src/App.java
+++ src/App.java
@@ -6,32 +6,45 @@ public class App {
     public static void main(String[] args) throws Exception {
         Locale.setDefault(Locale.US);
         Scanner scan = new Scanner(System.in);
-        double primeira, segunda, media;
+        double primeira, segunda, media, nota;
 
-        primeira = 0;
-        segunda = 0;
-        
+        primeira = 0.0;
+        segunda = 0.0;
+        media = 0.0;
+
         while (true) {
-            double numero;
-
-            numero = scan.nextDouble();
-            scan.nextLine();
-
-            if (numero < 0 || numero > 10) {
+            nota = scan.nextDouble();
+            //scan.nextLine();
+            if (nota < 0 || nota > 10) {
                 System.out.println("nota invalida");
                 continue;
             }
 
-            if (primeira == 0) {
-                primeira = numero;
-            } else {
-                segunda = numero;
-                break;
+            if (primeira == 0.0) {
+                primeira = nota;
+            } else if (segunda == 0.0) {
+                segunda = nota;
+                media = (double) (primeira + segunda) / 2;
+            }
+
+            if (media > 0) {
+                System.out.printf("media = %.2f%n", media);
+                while (true) {
+                    System.out.println("novo calculo (1-sim 2-nao)");
+                    int escolha = scan.nextInt();
+                    scan.nextLine();
+                    if (escolha == 1) {
+                        primeira = 0.0;
+                        segunda = 0.0;
+                        media = 0.0;
+                        break;
+                    } else if (escolha < 1 || escolha > 2) {
+                        continue;
+                    } else if(escolha == 2) {
+                        System.exit(0);
+                    }
+                }
             }
         }
-
-        media = (double) (primeira + segunda) / 2;
-
-        System.out.printf("media = %.2f%n", media);
     }
 }
