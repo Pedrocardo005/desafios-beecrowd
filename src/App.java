86a55b0b290b5304cdd02ea3947f57bcb5c26c31
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-05-20 23:01:28 -0300

    Resolve a 1131

--- src/App.java
+++ src/App.java
@@ -6,45 +6,51 @@ public class App {
     public static void main(String[] args) throws Exception {
         Locale.setDefault(Locale.US);
         Scanner scan = new Scanner(System.in);
-        double primeira, segunda, media, nota;
+        int inter, gremio, empates, grenais = 0;
 
-        primeira = 0.0;
-        segunda = 0.0;
-        media = 0.0;
+        inter = 0;
+        gremio = 0;
+        empates = 0;
 
         while (true) {
-            nota = scan.nextDouble();
-            //scan.nextLine();
-            if (nota < 0 || nota > 10) {
-                System.out.println("nota invalida");
+            int novoGrenal, golsInter, golsGremio = 0;
+            String resultado = scan.nextLine();
+            String[] resultadoArray = resultado.split(" ");
+
+            golsInter = Integer.parseInt(resultadoArray[0]);
+            golsGremio = Integer.parseInt(resultadoArray[1]);
+
+            if (golsInter > golsGremio) {
+                inter++;
+            } else if (golsGremio > golsInter) {
+                gremio++;
+            } else {
+                empates++;
+            }
+
+            grenais++;
+
+            System.out.println("Novo grenal (1-sim 2-nao)");
+            novoGrenal = scan.nextInt();
+            scan.nextLine();
+            if (novoGrenal == 1) {
                 continue;
+            } else {
+                break;
             }
+        }
 
-            if (primeira == 0.0) {
-                primeira = nota;
-            } else if (segunda == 0.0) {
-                segunda = nota;
-                media = (double) (primeira + segunda) / 2;
-            }
+        System.out.println(grenais + " grenais");
+        System.out.println("Inter:" + inter);
+        System.out.println("Gremio:" + gremio);
+        System.out.println("Empates:" + empates);
 
-            if (media > 0) {
-                System.out.printf("media = %.2f%n", media);
-                while (true) {
-                    System.out.println("novo calculo (1-sim 2-nao)");
-                    int escolha = scan.nextInt();
-                    scan.nextLine();
-                    if (escolha == 1) {
-                        primeira = 0.0;
-                        segunda = 0.0;
-                        media = 0.0;
-                        break;
-                    } else if (escolha < 1 || escolha > 2) {
-                        continue;
-                    } else if(escolha == 2) {
-                        System.exit(0);
-                    }
-                }
-            }
+        if (inter > gremio) {
+            System.out.println("Inter venceu mais");
+        } else if (gremio > inter) {
+            System.out.println("Gremio venceu mais");
+        } else {
+            System.out.println("Nao houve vencedor");
         }
     }
 }
