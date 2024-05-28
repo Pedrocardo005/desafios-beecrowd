c7518d16bb6f673f09d92b2a133cacc661dfab00
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-05-27 22:22:34 -0300

    Resolve a 1144

--- src/App.java
+++ src/App.java
@@ -4,17 +4,21 @@ public class App {
 
     public static void main(String[] args) throws Exception {
         Scanner scan = new Scanner(System.in);
-        int entrada, a, b, c;
-        entrada = scan.nextInt();
-        a = 1;
-        b = 1;
-        c = 1;
+        int n = scan.nextInt();
+        int a = 0;
+        int b = 1;
+        int c = 0;
         
-        for (int i = 0; i < entrada; i++) {
+        for (int x = 0; x < n * 2; x++) {
+            if (x % 2 == 0) {
+                a++;
+                b = a * a;
+                c = a * b;
+            } else {
+                b++;
+                c++;
+            }
             System.out.printf("%d %d %d%n", a, b, c);
-            a++;
-            b = a * a;
-            c = a * b;
         }
     }
 }
