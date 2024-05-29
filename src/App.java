af7f5c9b46c9cfb018a87049f87dfa7670a9452f
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-05-28 22:33:55 -0300

    Tenta resolver a 1145 mas sem sucesso ainda

--- src/App.java
+++ src/App.java
@@ -4,21 +4,25 @@ public class App {
 
     public static void main(String[] args) throws Exception {
         Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        int a = 0;
-        int b = 1;
-        int c = 0;
+        int x, y, a, b, c;
+        String linha;
+        String[] linhas;
         
-        for (int x = 0; x < n * 2; x++) {
-            if (x % 2 == 0) {
-                a++;
-                b = a * a;
-                c = a * b;
-            } else {
-                b++;
-                c++;
-            }
+        linha = scan.nextLine();
+        linhas = linha.split(" ");
+        a = 1;
+        b = 2;
+        c = 3;
+        
+        x = Integer.parseInt(linhas[0]);
+        y = Integer.parseInt(linhas[1]);
+        
+        while(a <= y && b <= y && c <= y) {
             System.out.printf("%d %d %d%n", a, b, c);
+            
+            a += x;
+            b += x;
+            c += x;
         }
     }
 }
