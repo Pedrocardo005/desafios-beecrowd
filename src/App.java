7b22087c87c82b508d53272ba6e91e1f0fef3130
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-05-29 23:57:30 -0300

    resolve a 1146

--- src/App.java
+++ src/App.java
@@ -4,25 +4,14 @@ public class App {
 
     public static void main(String[] args) throws Exception {
         Scanner scan = new Scanner(System.in);
-        int x, y, a, b, c;
-        String linha;
-        String[] linhas;
-        
-        linha = scan.nextLine();
-        linhas = linha.split(" ");
-        a = 1;
-        b = 2;
-        c = 3;
-        
-        x = Integer.parseInt(linhas[0]);
-        y = Integer.parseInt(linhas[1]);
-        
-        while(a <= y && b <= y && c <= y) {
-            System.out.printf("%d %d %d%n", a, b, c);
-            
-            a += x;
-            b += x;
-            c += x;
+        int numero = scan.nextInt();
+        while(numero != 0) {
+            String texto = "1";
+            for (int x = 2; x <= numero; x++) {
+                texto += " " + x;
+            }
+            System.out.println(texto);
+            numero = scan.nextInt();
         }
     }
 }
