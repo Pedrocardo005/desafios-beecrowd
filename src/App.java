abdc42ef2c4e5951b7341affd304e0376f2f9538
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-06-06 22:32:30 -0300

    Resolve a 1153

--- src/App.java
+++ src/App.java
@@ -5,24 +5,10 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
-        int[] lista = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            if (i == 0) {
-                lista[i] = 0;
-            } else if (i == 1) {
-                lista[i] = 1;
-            } else {
-                lista[i] = lista[i - 1] + lista[i - 2];
-            }
+        int multiplicacao = 1;
+        for(int x = 1; x <= n; x++) {
+            multiplicacao *= x;
         }
-        
-        for (int i = 0; i < lista.length; i++) {
-            if (i < lista.length - 1) {
-                System.out.printf("%d ", lista[i]);
-            } else {
-                System.out.printf("%d%n", lista[i]);
-            }
-        }
+        System.out.println(multiplicacao);
     }
 }
