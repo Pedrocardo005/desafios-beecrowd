8ec0df4924960b429b0fc59629aa527e518e8434
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-06-11 22:48:20 -0300

    Resolve a 1156 S II

--- src/App.rb
+++ src/App.rb
@@ -1,12 +1,13 @@
 s = 1.0
-numerador = 3
-divisor = 2
+numerador = 3.0
+divisor = 2.0
 
-while (numerador <= 39) {
-  s += (numerador / divisor.to_f)
+while numerador <= 39
+  s += numerador / divisor.to_f
 
-  numerador += 2
-  divisor *= 2
-}
+  numerador += 2.0
+  divisor *= 2.0
+end
 
-puts "#{s.round(2)}"
+numero = sprintf('%.2f', s)
+puts numero
