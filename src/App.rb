0afb19289ac154adf5bd0fba6ec27ac2bec843f5
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-06-14 22:37:14 -0300

    Resolve a 1158

--- src/App.rb
+++ src/App.rb
@@ -1,11 +1,29 @@
-numero = gets.chomp.to_i
-divisor = 1
-
-while divisor <= numero
-  if numero % divisor == 0
-    puts divisor
+def funcao(numero, qtd_somas)
+  numero_privado = numero
+  soma = 0
+  while qtd_somas > 0
+    if numero_privado % 2 != 0
+      soma += numero_privado
+      qtd_somas = qtd_somas - 1
+    end
+    numero_privado = numero_privado + 1
   end
-
-  divisor += 1
+  soma
 end
 
+qtd_vezes=gets.chomp.to_i
+
+resultados = []
+
+while qtd_vezes > 0
+  line = gets.chomp
+  nums = line.split(" ")
+  num_1 = nums[0].to_i
+  num_2 = nums[1].to_i
+  resultados.push(funcao(num_1, num_2))
+  qtd_vezes = qtd_vezes - 1
+end
+result_sem_um = resultados.length - 1
+for x in 0..result_sem_um
+  puts "#{resultados[x]}"
+end
