30c1108f461077793e90e5e9005c52a08830bf8f
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-06-20 23:37:46 -0300

    Resolve a 1159

--- src/App.rb
+++ src/App.rb
@@ -1,29 +1,23 @@
-def funcao(numero, qtd_somas)
-  numero_privado = numero
-  soma = 0
-  while qtd_somas > 0
-    if numero_privado % 2 != 0
-      soma += numero_privado
-      qtd_somas = qtd_somas - 1
+entrada = gets.chomp.to_i
+pares = []
+posicao_array = 0
+
+while entrada != 0
+  pares_encontrados = 0
+  _valores = entrada
+  while pares_encontrados < 5
+    if _valores % 2 == 0
+      if pares[posicao_array].nil?
+        pares[posicao_array] = _valores
+      else
+        pares[posicao_array] += _valores
+      end
+      pares_encontrados += 1
     end
-    numero_privado = numero_privado + 1
+    _valores += 1
   end
-  soma
-end
 
-qtd_vezes=gets.chomp.to_i
-
-resultados = []
-
-while qtd_vezes > 0
-  line = gets.chomp
-  nums = line.split(" ")
-  num_1 = nums[0].to_i
-  num_2 = nums[1].to_i
-  resultados.push(funcao(num_1, num_2))
-  qtd_vezes = qtd_vezes - 1
-end
-result_sem_um = resultados.length - 1
-for x in 0..result_sem_um
-  puts "#{resultados[x]}"
+  puts pares[posicao_array]
+  posicao_array += 1
+  entrada = gets.chomp.to_i
 end
