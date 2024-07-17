4b8a9655d43d0bc28e9e36ae17b150dfd6e88d30
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-07-17 20:33:55 -0300

    Comeca a fazer o 1022 em python

--- src/App.py
+++ src/App.py
@@ -1,10 +1,54 @@
+def adicao(n1:int, d1:int, n2:int, d2:int):
+    numerador = (n1 * d2 + n2 * d1)
+    denominador = d1 * d2
+    return '{}/{}'.format(numerador, denominador)
+
+def subtracao(n1:int, d1:int, n2:int, d2:int):
+    ...
+
+def multiplicacao(n1:int, d1:int, n2:int, d2:int):
+    ...
+
+def divisao(n1:int, d1:int, n2:int, d2:int):
+    ...
+
+def transform(elementos: list):
+    n1 = int(elementos[0])
+    d1 = int(elementos[2])
+    n2 = int(elementos[4])
+    d2 = int(elementos[6])
+
+    return [n1, d1, n2, d2]
+
+def simplificar(resultado: str):
+    ...
+
 x = int(input())
-vetor = [0, 0,0,0,0,0,0,0,0,0]
 
-vetor[0] = x
+lista = []
 
-for n in range(1, 10):
-    vetor[n] = vetor[n - 1] * 2
+for n in range(0, x):
+    leitura = input()
+    """
+    elementos[0] numerador
+    elementos[1] barra
+    elementos[2] denominador
+    elementos[3] operação
+    elementos[4] numerador 2
+    elementos[5] barra
+    elementos[6] denominador 2
+    """
+    elementos = leitura.split(' ')
+    n1, d1, n2, d2 = transform(elementos)
 
-for n in range(0, 10):
-    print('N[{}] = {}'.format(n, vetor[n]))
+    if elementos[3] == '+':
+        resultado = adicao(n1, d1, n2, d2)
+    
+    elif elementos[3] == '-':
+        resultado = subtracao(n1, d1, n2, d2)
+
+    elif elementos[3] == '*':
+        resultado = multiplicacao(n1, d1, n2, d2)
+
+    elif elementos[3] == '/':
+        resultado = divisao(n1, d1, n2, d2)
