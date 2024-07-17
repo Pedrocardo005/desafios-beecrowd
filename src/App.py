9c006bf1fd5ed72ccb9eb5d965ac8685447a85ee
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-07-16 21:58:29 -0300

    Resolve a 1173 com python

--- src/App.py
+++ src/App.py
@@ -1,9 +1,10 @@
-x = [None] * 10
+x = int(input())
+vetor = [0, 0,0,0,0,0,0,0,0,0]
 
-for number in range(0, 10):
-    valor = int(input())
+vetor[0] = x
 
-    if valor < 1:
-        valor = 1
-    x[number] = valor
-    print(f'X[{number}] = {x[number]}')
+for n in range(1, 10):
+    vetor[n] = vetor[n - 1] * 2
+
+for n in range(0, 10):
+    print('N[{}] = {}'.format(n, vetor[n]))
