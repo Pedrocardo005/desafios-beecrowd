68201c45a20c9dc33e10ff24965fa9c43b5a7d23
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-07-24 20:10:06 -0300

    Resolve a 1149 em python definitivamente

--- src/App.py
+++ src/App.py
@@ -1,10 +1,19 @@
-a = int(float(input()))
-n = 0
-while True:
-    n = int(float(input()))
+listagem = input()
 
-    if n > 0:
+numeros = listagem.split(' ')
+
+a = int(numeros[0])
+
+del numeros[0]
+
+n = 0
+
+for num in numeros:
+    current_num = int(num)
+    if current_num > 0:
+        n = current_num
         break
+
 somatorio = 0
 for x in range(0, n):
 
