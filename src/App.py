03b13e3431bd9dd7c56c8a69346af5c62d633a8d
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-07-25 22:52:52 -0300

    Resolve a 1175

--- src/App.py
+++ src/App.py
@@ -1,22 +1,6 @@
-listagem = input()
+lista = []
+for index in range(0, 20):
+    lista.insert(0, int(input()))
 
-numeros = listagem.split(' ')
-
-a = int(numeros[0])
-
-del numeros[0]
-
-n = 0
-
-for num in numeros:
-    current_num = int(num)
-    if current_num > 0:
-        n = current_num
-        break
-
-somatorio = 0
-for x in range(0, n):
-
-    somatorio = somatorio + a + x
-
-print(somatorio)
+for index in range(0, 20):
+    print('N[{}] = {}'.format(index, lista[index]))
