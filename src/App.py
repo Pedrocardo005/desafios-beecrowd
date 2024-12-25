abd1c384b9a86a0b77f31c02022656b418f6819e
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-12-25 14:33:10 -0300

    Tentando 1534 com python

--- src/App.py
+++ src/App.py
@@ -1,47 +1,43 @@
 
 
-def resolve_question(dados: str):
+def resolve_question(valor_matriz: int):
     """
-    >>> resolve_question('100 150 1.0 0')
-    '51 anos.'
-    >>> resolve_question('90000 120000 5.5 3.5')
-    '16 anos.'
-    >>> resolve_question('56700 72000 5.2 3.0')
-    '12 anos.'
-    >>> resolve_question('123 2000 3.0 2.0')
-    'Mais de 1 seculo.'
-    >>> resolve_question('100000 110000 1.5 0.5')
-    '10 anos.'
-    >>> resolve_question('62422 484317 3.1 1.0')
-    '100 anos.'
+    >>> resolve_question(4)
+    1332
+    3123
+    3213
+    2331
+    >>> resolve_question(7)
+    1333332
+    3133323
+    3313233
+    3332333
+    3323133
+    3233313
+    2333331
     """
+    idx_um = 0
+    idx_dois = valor_matriz - 1
+    matriz = []
+    for x in range(0, valor_matriz):
+        matriz.append([])
+        for y in range(0, valor_matriz):
+            if y == idx_dois:
+                matriz[x].append('2')
+            elif y == idx_um:
+                matriz[x].append('1')
+            else:
+                matriz[x].append('3')
+        idx_um += 1
+        idx_dois -= 1
 
-    valores = dados.split(' ')
-    pa = int(valores[0])  # População A
-    pb = int(valores[1])  # População B
-    g1 = float(valores[2])  # Taxa de crescimento A (%)
-    g2 = float(valores[3])  # Taxa de crescimento B (%)
-
-    anos = 0
-    while pa <= pb:
-        # Atualiza populações com as taxas de crescimento
-        pa += int(pa * (g1 / 100))
-        pb += int(pb * (g2 / 100))
-
-        anos += 1
-
-        # Limita o número de anos a 100
-        if anos > 100:
-            return 'Mais de 1 seculo.'
-
-    return f'{anos} anos.'
+    for linha in matriz:
+        print(''.join(linha))
 
 
-x = int(input())
-for _ in range(0, x):
-    valor = input()
-    print(resolve_question(valor))
+valor = int(input())
+resolve_question(valor)
 
 # if __name__ == "__main__":
-#    import doctest
-#    doctest.testmod()
+#     import doctest
+#     doctest.testmod()
