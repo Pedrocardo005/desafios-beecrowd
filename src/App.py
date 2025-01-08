009b00607899e6eeeeee580e2f89cd54fb74472c
Pedrocardo005 <pedrocardo007@hotmail.com>
2025-01-07 23:54:16 -0300

    Tentando a 1557 em python

--- src/App.py
+++ src/App.py
@@ -1,42 +1,48 @@
-
-
 def resolve_question(valor_matriz: int):
     """
+    >>> resolve_question(1)
+    1
+    >>> resolve_question(2)
+    1 2
+    2 4
+    >>> resolve_question(3)
+    1  2  4
+    2  4  8
+    4  8 16
     >>> resolve_question(4)
-    1332
-    3123
-    3213
-    2331
-    >>> resolve_question(7)
-    1333332
-    3133323
-    3313233
-    3332333
-    3323133
-    3233313
-    2333331
+    1  2  4  8
+    2  4  8 16
+    4  8 16 32
+    8 16 32 64
+    >>> resolve_question(5)
+    1   2   4   8  16
+    2   4   8  16  32
+    4   8  16  32  64
+    8  16  32  64 128
+    16  32  64 128 256
     """
-    idx_um = 0
-    idx_dois = valor_matriz - 1
-    matriz = []
+    matriz = [[None] * valor_matriz] * valor_matriz
+
+    initial_x = 1
     for x in range(0, valor_matriz):
-        matriz.append([])
+        initial_y = initial_x
         for y in range(0, valor_matriz):
-            if y == idx_dois:
-                matriz[x].append('2')
-            elif y == idx_um:
-                matriz[x].append('1')
-            else:
-                matriz[x].append('3')
-        idx_um += 1
-        idx_dois -= 1
+            matriz[x][y] = str(initial_y)
+            initial_y += initial_y
+        initial_x += initial_x
 
-    for linha in matriz:
-        print(''.join(linha))
+    for x in range(0, valor_matriz):
+        print(" ".join(matriz[x]))
+
+    print()
 
 
-valor = int(input())
-resolve_question(valor)
+while True:
+    valor = int(input())
+    if valor != 0:
+        resolve_question(valor)
+    else:
+        break
 
 # if __name__ == "__main__":
 #     import doctest
