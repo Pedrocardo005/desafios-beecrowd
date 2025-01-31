e711449cec36f4a44337ee946fa8aa009be17a03
Pedrocardo005 <pedrocardo007@hotmail.com>
2025-01-30 22:41:40 -0300

    Resolve a 1564 com python

--- src/App.py
+++ src/App.py
@@ -1,52 +1,33 @@
-def resolve_question(valor_matriz: int):
+def resolve_question(parametro: int):
     """
+    >>> resolve_question(0)
+    'vai ter copa!'
     >>> resolve_question(1)
-    1
+    'vai ter duas!'
+    >>> resolve_question(0)
+    'vai ter copa!'
     >>> resolve_question(2)
-    1 2
-    2 4
-    >>> resolve_question(3)
-    1  2  4
-    2  4  8
-    4  8 16
-    >>> resolve_question(4)
-    1  2  4  8
-    2  4  8 16
-    4  8 16 32
-    8 16 32 64
-    >>> resolve_question(5)
-    1   2   4   8  16
-    2   4   8  16  32
-    4   8  16  32  64
-    8  16  32  64 128
-    16  32  64 128 256
+    'vai ter duas!'
+    >>> resolve_question(100)
+    'vai ter duas!'
+    >>> resolve_question(0)
+    'vai ter copa!'
     """
-    matriz = [[None] * valor_matriz] * valor_matriz
-
-    initial_x = 1
-    for x in range(0, valor_matriz):
-        initial_y = initial_x
-        for y in range(0, valor_matriz):
-            matriz[x][y] = str(initial_y)
-            initial_y += initial_y
-        initial_x += initial_x
-
-    len_spaces = len(str(initial_x))
-
-    for x in range(0, valor_matriz):
-        for x in range(0, len(matriz[x])):
-            len_spaces
-
-    print()
-
-
-while True:
-    valor = int(input())
-    if valor != 0:
-        resolve_question(valor)
+    if parametro > 0:
+        return "vai ter duas!"
     else:
-        break
+        return "vai ter copa!"
+
+
+try:
+    while True:
+        valor = int(input())
+
+        print(resolve_question(valor))
+except:
+    ...
 
 # if __name__ == "__main__":
-#     import doctest
-#     doctest.testmod()
+#    import doctest
+
+#    doctest.testmod()
