ad964bc1f2584839beb1d77ee6be02ec856728cb
Pedrocardo005 <pedrocardo007@hotmail.com>
2025-02-05 19:26:09 -0300

    Tentando resolver a 1478 em python

--- src/App.py
+++ src/App.py
@@ -1,33 +1,66 @@
-def resolve_question(parametro: int):
+def resolve_question(numero: int):
     """
-    >>> resolve_question(0)
-    'vai ter copa!'
     >>> resolve_question(1)
-    'vai ter duas!'
-    >>> resolve_question(0)
-    'vai ter copa!'
+      1
     >>> resolve_question(2)
-    'vai ter duas!'
-    >>> resolve_question(100)
-    'vai ter duas!'
-    >>> resolve_question(0)
-    'vai ter copa!'
+      1   2
+      2   1
+    >>> resolve_question(3)
+      1   2   3
+      2   1   2
+      3   2   1
+    >>> resolve_question(4)
+      1   2   3   4
+      2   1   2   3
+      3   2   1   2
+      4   3   2   1
+    >>> resolve_question(5)
+      1   2   3   4   5
+      2   1   2   3   4
+      3   2   1   2   3
+      4   3   2   1   2
+      5   4   3   2   1
     """
-    if parametro > 0:
-        return "vai ter duas!"
+    matriz = [[None] * numero for _ in range(0, numero)]
+
+    for x in range(0, numero):
+        inc = False
+        current_val = x + 1
+        for y in range(0, numero):
+
+            if current_val == 1:
+                inc = True
+
+            matriz[x][y] = current_val
+
+            if inc:
+                current_val += 1
+            else:
+                current_val -= 1
+
+    for x in range(0, numero):
+        for y in range(0, numero):
+            if y == 0:
+                print(f'  {matriz[x][y]}', end="")
+            else:
+                print(f'   {matriz[x][y]}', end="")
+
+        print()
+
+
+numeros = []
+
+while True:
+    n = int(input())
+
+    if n != 0:
+        numeros.append(n)
+
     else:
-        return "vai ter copa!"
+        break
 
-
-try:
-    while True:
-        valor = int(input())
-
-        print(resolve_question(valor))
-except:
-    ...
+[resolve_question(numero) for numero in numeros]
 
 # if __name__ == "__main__":
 #    import doctest
-
 #    doctest.testmod()
